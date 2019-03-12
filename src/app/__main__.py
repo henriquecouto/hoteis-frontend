655cda8ee43b3c634042ded8d293a4b23127229a
Henrique Couto <henrique.coutu@live.com>
2019-03-11 21:17:11 -0300

    resolvendo erro

--- src/app/__main__.py
+++ src/app/__main__.py
@@ -134,7 +134,8 @@ def infoQuarto(numero):
 
     return template(views+'quarto.tpl', quarto=quarto, reservas=reservas)
 
-if os.environ.get('APP_LOCATION') == 'heroku':
-    root.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
-else:
-    root.run(host='localhost', port=8081, debug=True, reloader=True)
+#heroku
+root.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+
+#Local
+# root.run(host='localhost', port=8081, debug=True, reloader=True)
